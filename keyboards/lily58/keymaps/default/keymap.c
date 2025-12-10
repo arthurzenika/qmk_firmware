3fcc9489e549803b7ffc7ca593a73557cc46f363
Arthur Lutz <arthur.lutz@zenika.com>
2025-12-10 11:23:04 +0100

    feat(lily58): shift not used, put backspace

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
-                        KC_LALT, MO(_LOWER), KC_LSFT, KC_BSPC, KC_SPC, KC_ENT, MO(_RAISE), KC_ALGR
+                        KC_LALT, MO(_LOWER), KC_BSPC, KC_BSPC, KC_SPC, KC_ENT, MO(_RAISE), KC_ALGR
 ),
 // KC_LGUI to readd
 /* LOWER
@@ -67,7 +67,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   _______, KC_CIRC,  KC_LT,   KC_GT, KC_DLR, KC_PERC,                        KC_AT,    KC_AMPR,    KC_ASTR,    KC_QUOT,    KC_GRV,    _______,
